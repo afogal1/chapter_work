b7586cfdf2e4ace52f6555388fd437034d47e1bd
afogal1 <alex.r.fogal@gmail.com>
2021-12-30 12:25:28 -0600

    Chapter 7

---
+++ chapter7/video_work.py
@@ -0,0 +1,24 @@
+# the while loop
+# while Boolean or Bollean expression:
+    # code block
+    # condition is true 
+
+
+# You must be over 5 feet to ride my ride
+# I have a magic potion that will let you grow one inch everytime you use it!
+height = 55 
+while height < 60:
+    print(f"You are {height} inches tall \n and too short to ride!")
+    print("Drink my magic potion")
+    height += 1
+
+print("Thanks for coming")
+
+# input function
+height1 = input("What is your height in inches? ")
+while height1 < 60:
+    print(f"You are {height1} inches tall \n and too short to ride!")
+    print("Drink my magic potion")
+    height1 += 1
+
+print("Thanks for coming")
